707ba1f00262caa34670367d545c4317d705dcbd
Matheus Alves <malves224@gmail.com>
2021-06-23 19:17:22 -0300

    cria variavel que multiplica a com b e imprime resultado em console

--- exercises/bloco_4/introducao-a-javascript-e-logica-de-programacao_1./operacoesAritmetica.js
+++ exercises/bloco_4/introducao-a-javascript-e-logica-de-programacao_1./operacoesAritmetica.js
@@ -1,8 +1,16 @@
 // primeiro programa 
+
 let a = 6;
 let b = 4;
 let soma = (a + b);
 console.log(soma);
+
 // segundo programa
+
 let subtracao = (a - b);
 console.log(subtracao);
+
+// terceiro programa
+
+let multiplicacao = (a * b);
+console.log(multiplicacao);
