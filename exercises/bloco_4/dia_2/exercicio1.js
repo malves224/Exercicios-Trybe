4aa24c8d6ce86d19d63c34349d18f0e68e45b5b9
Matheus Alves <malves224@gmail.com>
2021-06-24 19:32:34 -0300

    resolve requisito 4 do exercicio

--- exercises/bloco_4/dia_2/exercicio1.js
+++ exercises/bloco_4/dia_2/exercicio1.js
@@ -3,5 +3,10 @@ let mediaAritmetica = null;
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 for (let i = 0; i < numbers.length; i += 1){
     soma = (numbers[i]) + soma;
-    mediaAritmetica = soma / numbers.length;
-} console.log(mediaAritmetica);
+} 
+mediaAritmetica = soma / numbers.length;
+if (mediaAritmetica > 20) {
+    console.log("Valor Maior que 20")
+}else if(mediaAritmetica <= 20){
+    console.log("valor menor ou igual a 20")
+}
