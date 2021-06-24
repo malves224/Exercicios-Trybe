0eab5407ae2f9cb4f70a8ae742343740f1a01152
Matheus Alves <malves224@gmail.com>
2021-06-24 19:24:39 -0300

    resolve requisito 3 do exercicio

--- exercises/bloco_4/dia_2/exercicio1.js
+++ exercises/bloco_4/dia_2/exercicio1.js
@@ -1,5 +1,7 @@
 let soma = null;
+let mediaAritmetica = null;
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 for (let i = 0; i < numbers.length; i += 1){
     soma = (numbers[i]) + soma;
-} console.log(soma);
+    mediaAritmetica = soma / numbers.length;
+} console.log(mediaAritmetica);
