2f6b20fa17ae1c142c77704b0e589ad5a7dc206f
Matheus Alves <malves224@gmail.com>
2021-06-24 11:54:45 -0300

    Cria estrutura condicional para solucionar o exercicio 4

--- exercises/bloco_4/negativoOuPositivo.js
+++ exercises/bloco_4/negativoOuPositivo.js
@@ -1,2 +1,9 @@
-var saldoEmBanco = -6;
+let saldoEmBanco = 0;
 
+if (saldoEmBanco > 0){
+    console.log("saldo em banco: Positivo" )
+} else if (saldoEmBanco < 0){
+    console.log("saldo em banco: negativo" )
+} else {
+    console.log("zero")
+}
