7e7720769e7062eef1c2ae7436e4ff01054fe487
Matheus Alves <malves224@gmail.com>
2021-06-24 15:48:54 -0300

    cria estrutura condicional para saber valor nominal do inss

--- exercises/bloco_4/introducao-a-javascript-e-logica-de-programacao_1./LucroASerRecebido.js
+++ exercises/bloco_4/introducao-a-javascript-e-logica-de-programacao_1./LucroASerRecebido.js
@@ -1,9 +1,12 @@
-let salarioBruto = 1500.10
-let aliquotaInss = null;
+let salarioBruto = 5189.82;
+let valorInss = null;
 
-if (salarioBruto > 0 && salarioBruto < 1556.94){
-    aliquotaInss = 0.08;
-} else if (salarioBruto > 1.556,95 && salarioBruto <= 2.594,92) {
-    aliquotaInss = 0.09;
+if (salarioBruto > 0 && salarioBruto <= 1556.94){
+    valorInss = salarioBruto * 0.08;
+} else if (salarioBruto > 1556.95 && salarioBruto <= 2594.92) {
+    valorInss = salarioBruto * 0.09;
+}else if (salarioBruto > 2594.93 && salarioBruto <= 5189.82) {
+    valorInss = salarioBruto * 0.11;
+} else if (salarioBruto > 5189.82) {
+    valorInss = 570.88
 }
-console.log(aliquotaInss);
