eba77990c90c6d30126dcaddd4d24c89f823fca2
Matheus Alves <malves224@gmail.com>
2021-06-24 12:58:03 -0300

    adiciona mais case com o restantes das peças do xadrez

--- exercises/bloco_4/introducao-a-javascript-e-logica-de-programacao_1./pecaDeXadrez.js
+++ exercises/bloco_4/introducao-a-javascript-e-logica-de-programacao_1./pecaDeXadrez.js
@@ -1,6 +1,21 @@
 let pecaDeXadrez = "CAVALO".toLocaleLowerCase();
 
 switch(pecaDeXadrez) {
-    case "cavalo":
-    console.log ("movimento em L");
-}
+	case "cavalo":
+		console.log ("movimento em L");
+		break;
+	case "torre":
+		console.log ("movimento em linha reta, sem limite de casas desde que haja espaço livre");
+		break;	
+	case "bispo":
+		console.log ("Movimento sempre em diagonal");
+		break;
+	case "peão":
+		console.log ("movimento para frente, e captura em diagonal");
+		break;
+	case "rei": 
+		console.log ("movimeto para todos lados, limitado a uma casa");
+	case "rainha":
+		console.log ("movimento para todos os lados, sem limites de casa");
+		break;
+	}	
