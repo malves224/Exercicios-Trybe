751cccfd89cae91009970855dc0e06f3f135c27f
Matheus Alves <malves224@gmail.com>
2021-06-24 15:36:24 -0300

    cria estrutura condiional para descobrir aliquota inss

--- exercises/bloco_4/introducao-a-javascript-e-logica-de-programacao_1./LucroASerRecebido.js
+++ exercises/bloco_4/introducao-a-javascript-e-logica-de-programacao_1./LucroASerRecebido.js
@@ -1,1 +1,9 @@
-let salarioBruto = 1500;
+let salarioBruto = 1500.10
+let aliquotaInss = null;
+
+if (salarioBruto > 0 && salarioBruto < 1556.94){
+    aliquotaInss = 0.08;
+} else if (salarioBruto > 1.556,95 && salarioBruto <= 2.594,92) {
+    aliquotaInss = 0.09;
+}
+console.log(aliquotaInss);
