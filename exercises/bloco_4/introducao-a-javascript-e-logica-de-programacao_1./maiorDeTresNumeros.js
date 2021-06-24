0adaec2f8a1f00636b99086463fb1ad2da195bd7
Matheus Alves <malves224@gmail.com>
2021-06-24 11:27:52 -0300

    cria estrtura condicional para resolver o problema

---
+++ exercises/bloco_4/introducao-a-javascript-e-logica-de-programacao_1./maiorDeTresNumeros.js
@@ -0,0 +1,11 @@
+let num1 = 12;
+let num2 = 5;
+let num3 = 9;
+
+if (num1 > num2 && num1 > num3) {
+    console.log(num1);
+} else if (num2 > num1 && num2 > num3) {
+    console.log(num2);
+} else if (num3 > num1 && num3 > num2) {
+    console.log(num3);
+}
