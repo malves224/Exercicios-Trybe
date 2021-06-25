9e8f429c80df838364fe7d057a5ee8bb5c362128
Matheus Alves <malves224@gmail.com>
2021-06-25 19:24:21 -0300

    resolve exercicio 2 dos asteriscos

--- exercises/bloco_4/dia_3/exercicio1Asteriscos.js
+++ exercises/bloco_4/dia_3/exercicio1Asteriscos.js
@@ -3,6 +3,7 @@ let asterisco = '*';
 
 for (let index = 1; index < n; index += 1){
  asterisco += "*";
+ 
 } if (asterisco.length === n){
     for (let index = 1; index <= n; index += 1) {
         console.log(asterisco);
