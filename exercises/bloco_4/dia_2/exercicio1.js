356b40aca91d580c58a651c2c0508e6d24286d9b
Matheus Alves <malves224@gmail.com>
2021-06-24 23:03:24 -0300

    resolve exercicio 5 do exercicio

--- exercises/bloco_4/dia_2/exercicio1.js
+++ exercises/bloco_4/dia_2/exercicio1.js
@@ -1,12 +1,10 @@
 let soma = null;
 let mediaAritmetica = null;
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let maiorValor = null;
 for (let i = 0; i < numbers.length; i += 1){
-    soma = (numbers[i]) + soma;
-} 
-mediaAritmetica = soma / numbers.length;
-if (mediaAritmetica > 20) {
-    console.log("Valor Maior que 20")
-}else if(mediaAritmetica <= 20){
-    console.log("valor menor ou igual a 20")
-}
+        if (maiorValor < numbers[i]){
+          maiorValor = (numbers[i])
+        } 
+    }
+console.log(maiorValor);
