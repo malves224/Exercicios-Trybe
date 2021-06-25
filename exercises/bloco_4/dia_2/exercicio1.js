ef6f5dfba02d632de6e4ee435b9bdab5e0d8dd91
Matheus Alves <malves224@gmail.com>
2021-06-25 14:58:06 -0300

    resolve o problema do exercicio 7inserindo a primeira posiçao na variavel 1

--- exercises/bloco_4/dia_2/exercicio1.js
+++ exercises/bloco_4/dia_2/exercicio1.js
@@ -1,16 +1,11 @@
 let soma = null;
 let mediaAritmetica = null;
-let numbers = [1, 8, 43, 23, 70, 8, 100, 8, 36, 298];
-let contadorDeImpares = null;
+let numbers = [1, 8, 43, 23, 2, 7, 100, 55, 7, 28];
+let numeroMenor = numbers[0];
 for (let i = 0; i < numbers.length; i += 1){
-        if (numbers[i] % 2 === 1){
-          contadorDeImpares += 1;
-        } else if (contadorDeImpares === 0) {
-          contadorDeImpares += 0
-        }
-    }
-if (contadorDeImpares > 0){
-  console.log(contadorDeImpares)
-} else {
-  console.log("nao ha numeros impares")
-}
+
+      if (numbers[i] < numeroMenor){
+        numeroMenor = numbers[i];
+    }    
+  }
+console.log(numeroMenor)
