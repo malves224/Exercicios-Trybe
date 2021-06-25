029f6decd7eef9db310f2127dad3d9463e2cd62b
Matheus Alves <malves224@gmail.com>
2021-06-25 15:51:37 -0300

    altera o arquivo com um desafio de ordenaçao

--- exercises/bloco_4/dia_2/exercicio1.js
+++
@@ -1,9 +0,0 @@
-for (let index = 1; index < array.length; index += 1) {
-  for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
-    if (array[index] < array[secondIndex]) {
-      let position = array[index];
-      array[index] = array[secondIndex];
-      array[secondIndex] = position;
-    }
-  }
-}
