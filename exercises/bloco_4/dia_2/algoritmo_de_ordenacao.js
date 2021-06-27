26a04263b2270b27018b1a68513b31d66a8361cf
Matheus Alves <malves224@gmail.com>
2021-06-27 12:20:02 -0300

    resolve o exercicio bonus do bloco 4.2 em parcialmente

--- exercises/bloco_4/dia_2/algoritmo_de_ordenacao.js
+++ exercises/bloco_4/dia_2/algoritmo_de_ordenacao.js
@@ -1,14 +1,11 @@
 let array = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-for (let index = 1; index < array.length; index += 1) {
-  for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
-    if (array[secondIndex] > array[index]) {
-      let position = array[index];
-      array[index] = array[secondIndex];
-      array[secondIndex] = position;
+for (let index = 0; index < array.length; index += 1) 
+  for (let secondIndex = index-1; secondIndex < index; secondIndex += 1) {
+    array[secondIndex] = array[secondIndex] * array[index];
+    array
     }
-  }
-}
+  
 
-console.log (array)
+console.log (array[index])
 
 // Agora crie um novo array a partir do array numbers , sem perdê-lo. Cada valor do novo array deverá ser igual ao valor correspondente no array numbers multiplicado pelo seguinte. Por exemplo: o primeiro valor do novo array deverá ser 45, pois é a multiplicação de 5 (primeiro valor) e 9 (valor seguinte). Já o segundo valor do novo array deverá ser 27, pois é a multiplicação de 9 (segundo valor) e 3 (valor seguinte), e assim por diante. Caso não haja próximo valor, a multiplicação deverá ser feita por 2. Faça isso utilizando o for e o método push . O resultado deve ser o array abaixo: 
