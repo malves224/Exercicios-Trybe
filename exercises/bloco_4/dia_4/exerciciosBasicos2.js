0379c82be542c31ce71be9a398ad4c5d8f1c999d
Matheus Alves <malves224@gmail.com>
2021-06-28 20:40:34 -0300

    resolvendo exercicio 2 de for/in

--- exercises/bloco_4/dia_4/exerciciosBasicos2.js
+++ exercises/bloco_4/dia_4/exerciciosBasicos2.js
@@ -1,9 +1,9 @@
-let names = {
-    person1:'João',
-    person2:'Maria',
-    person3:'Jorge' 
+let car = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
   };
 
-  for (let key in names){
-      console.log('Ola',    names[key])
+  for (key in car) {
+      console.log(key, car[key])
   }
