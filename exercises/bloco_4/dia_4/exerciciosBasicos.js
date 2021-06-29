46e725bf8d3457622f2e9587efb56348612fb4f9
Matheus Alves <malves224@gmail.com>
2021-06-28 21:10:11 -0300

    adiciona funçao para melhorar funcionamento

--- exercises/bloco_4/dia_4/exerciciosBasicos.js
+++ exercises/bloco_4/dia_4/exerciciosBasicos.js
@@ -1,4 +1,4 @@
-let player = {
+    let player = {
     nome: 'marta',
     lastName: 'silva',
     age: 34,
@@ -9,8 +9,16 @@ let player = {
     bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018]
 }
 
+function dadosDoAtleta(nome) {
+if (nome === player.nome)  {
 console.log('a jogadora ', player.nome, 'tem', player.age, 'anos de idade');
 
 console.log('a jogadora', player.nome, 'foi eleita a melhor do mundo por ', player.bestInTheWorld.length, 'vezes')
 
 console.log('a jogadora possui', player.medalhas.golden, 'medalhas de ouro e', player.medalhas.silver, 'de prata ')
+} else {
+    console.log("atleta nao econtrado")
+}
+}
+
+dadosDoAtleta ('');
