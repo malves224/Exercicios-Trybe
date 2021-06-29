98ba6801032b5d49e71c572869edf02559cbf90a
Matheus Alves <malves224@gmail.com>
2021-06-28 22:49:23 -0300

    resolve exercicio 1 parte II

---
+++ exercises/bloco_4/dia_4/exercicio-1PALINDROMO.js
@@ -0,0 +1,11 @@
+function verificaPalindromo (palavra){
+let palavraInvertida = '';
+for (let index = palavra.length-1; index > -1; index -= 1){
+    palavraInvertida = palavraInvertida + palavra[index]
+} if (palavra === palavraInvertida) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+}
+verificaPalindromo ("desenvolvimento")
