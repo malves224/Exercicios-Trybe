2fc16243df36023e91108720dc59de2bd171c155
Matheus Alves <malves224@gmail.com>
2021-07-02 23:28:15 -0300

    resolve exercicio 4 do bloco 4.5

--- exercises/bloco_4/dia_4/exercicio2VerificarMaiorValor.js
+++ exercises/bloco_4/dia_4/exercicio2VerificarMaiorValor.js
@@ -19,7 +19,7 @@ function highestCount(array) {
   return (output)
 }
 console.log(highestCount([9, 1, 9, 3, 9, 9, 7]))
-    
+ // exercicio 3 do bloco 4;5   
 function indiceMenorValor (array){
   let indexMenorValor = 0;
   for ( let index = 0; index < array.length; index += 1){
@@ -34,7 +34,7 @@ function indiceMenorValor (array){
 console.log(indiceMenorValor([-5, 2, 6, -10, 4, -10, -4]));
 
 
-// outra forma de resolver
+// outra forma de resolver exercicio 2 
 //let numerosInteiros = [500, 3,365, 100, 10, 600]
 //function maiorNumero (array){
 //    let maiorNumero = 0;
@@ -50,3 +50,16 @@ console.log(indiceMenorValor([-5, 2, 6, -10, 4, -10, -4]));
     
 //    console.log (maiorNumero);
 //}
+
+//exercicio 4 Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
+
+function MaiorCaracteres (array){
+  let MaiorCaracteres = array[0];
+  for (let Valor of array){
+    if (Valor.length > MaiorCaracteres.length){
+      MaiorCaracteres = Valor;
+    }
+  }
+  return MaiorCaracteres
+}
+console.log(MaiorCaracteres(['Fernanda', 'Lucas', 'Nádia', 'Cairo', ]));
