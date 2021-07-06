423b224aa1e7675948a2f8c59876b750f238651c
Matheus Alves <malves224@gmail.com>
2021-07-05 22:11:05 -0300

    estiliza a pagina usando javascript

---
+++ exercises/bloco_5/dia-2/administraDoTempo/script.js
@@ -0,0 +1,16 @@
+let header = document.querySelectorAll(".header-js");
+header[0].style.backgroundColor = '#2fc18c';
+header[1].style.backgroundColor = '#2fc18c'
+
+let urgencias = document.querySelectorAll(".emergencyJs")
+urgencias[0].style.backgroundColor = "#FE9A2E";
+urgencias[1].style.backgroundColor = "#FF0040";
+urgencias[2].style.backgroundColor = "#FF0040";
+
+let naoUrgencias = document.querySelectorAll(".noEmegencyJs");
+naoUrgencias[0].style.backgroundColor = "#DF01D7";
+naoUrgencias[1].style.backgroundColor = "#2E9AFE";
+naoUrgencias[2].style.backgroundColor = "#2E9AFE"
+
+let footer = document.querySelectorAll("footer")
+footer[0].style.backgroundColor = "#424242"
