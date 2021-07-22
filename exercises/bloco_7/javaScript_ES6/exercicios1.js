f70f6ba35028526e83a8f10154a3152a55a259b9
Matheus Alves <malves224@gmail.com>
2021-07-22 18:42:18 -0300

    modifica as concatenaçoes para template literals

--- exercises/bloco_7/javaScript_ES6/exercicios1.js
+++ exercises/bloco_7/javaScript_ES6/exercicios1.js
@@ -4,7 +4,7 @@
       ifScope = `${ifScope}  ótimo, fui utilizada no escopo !`;
       console.log(ifScope);
     } else {
-      let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+      let elseScope =  `Não devo ser utilizada fora meu escopo (else)`
       console.log(elseScope);
     }
   }
