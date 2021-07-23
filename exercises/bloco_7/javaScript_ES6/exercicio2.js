8039edf46300cc13b44c51ae9a2c6e010b2b8db2
Matheus Alves <malves224@gmail.com>
2021-07-23 11:50:56 -0300

    adiciona comentario para o exercicio que utiliza array.sort()

--- exercises/bloco_7/javaScript_ES6/exercicio2.js
+++ exercises/bloco_7/javaScript_ES6/exercicio2.js
@@ -1,7 +1,7 @@
 const oddsAndEvens = [10, 3, 4, 415, 45, 69, 45, 8, 7, 13];
 
 const ordenar = array => { 
-    const arrayOrdenado = array.sort((b, a) => a - b);
+    const arrayOrdenado = array.sort((b, a) => a - b); //ordenado decrescente, mude para a, b para ordenar crescente.
     return ` Os números ${arrayOrdenado} se encontram ordenados de forma crescente!`;
 }
 
