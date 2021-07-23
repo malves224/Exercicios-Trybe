ca58720e1c646fd0a40d28a850290022e7b23022
Matheus Alves <malves224@gmail.com>
2021-07-23 20:50:19 -0300

    soluçao requisito 1 do exercicio2

---
+++ exercises/bloco_7/objetos_js/exercicio2.js
@@ -0,0 +1,25 @@
+const lesson1 = {
+    materia: 'Matemática',
+    numeroEstudantes: 20,
+    professor: 'Maria Clara',
+    turno: 'manhã',
+  };
+  
+  const lesson2 = {
+    materia: 'História',
+    numeroEstudantes: 20,
+    professor: 'Carlos',
+  };
+  
+  const lesson3 = {
+    materia: 'Matemática',
+    numeroEstudantes: 10,
+    professor: 'Maria Clara',
+    turno: 'noite',
+  };
+
+  const addKey = (objeto, chave, valor) => objeto[chave] = valor;
+
+  addKey (lesson2, 'turno', 'manhã');
+
+  console.log(lesson2);
