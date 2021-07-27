766c744201ea6f99de3aaf487fe2f1d4e99aec0f
Matheus Alves <malves224@gmail.com>
2021-07-27 12:09:26 -0300

    adiciona exercicio para bloco 7.3 testes unitarios

--- exercises/bloco_7/javaScript_ES6/exercicio5.js
+++ exercises/bloco_7/javaScript_ES6/exercicio5.js
@@ -1,9 +1,10 @@
 const stringDeterminada = 'Tryber x aqui';
-const minhasSkills = ['HTML' , 'JAVASCRIPT' , 'CSS'];
-const replaceX = (string) => {
-    return stringDeterminada.replace('x', string);
-}
-const concatenaSkills = (funcao1) => {
-    return `${funcao1} Minhas habilidades principais são: ${minhasSkills.sort()} `;
-} 
+const minhasSkills = ['HTML', 
+'JAVASCRIPT' , 
+'CSS'];
+const replaceX = string => stringDeterminada.replace('x', string);
+
+const concatenaSkills = funcao1 => `${funcao1} Minhas habilidades principais são:
+     ${minhasSkills.sort()} `;
+
 console.log (concatenaSkills (replaceX('matheus')));
