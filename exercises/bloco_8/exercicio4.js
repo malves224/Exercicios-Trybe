6fe6cdb42860e9e004cf9d4cdff6355b2e42fddf
Matheus Alves <malves224@gmail.com>
2021-07-30 19:00:23 -0300

    soluçao exercicio 4

--- exercises/bloco_8/exercicio4.js
+++ exercises/bloco_8/exercicio4.js
@@ -61,3 +61,43 @@ const books = [{
 ];
 
 const assert = require('assert');
+const expectedResult = [
+    { 
+      id: 1,
+      name: 'As Crônicas de Gelo e Fogo',
+      genre: 'Fantasia',
+      author: { name: 'George R. R. Martin', birthYear: 1948 },
+      releaseYear: 1991
+    },
+    {
+      id: 2,
+      name: 'O Senhor dos Anéis',
+      genre: 'Fantasia',
+      author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+      releaseYear: 1954
+    },
+    {
+      id: 3,
+      name: 'Fundação',
+      genre: 'Ficção Científica',
+      author: { name: 'Isaac Asimov', birthYear: 1920 },
+      releaseYear: 1951
+    },
+    {
+      id: 4,
+      name: 'Duna',
+      genre: 'Ficção Científica',
+      author: { name: 'Frank Herbert', birthYear: 1920 },
+      releaseYear: 1965
+    }
+  ];
+  
+  function fantasyOrScienceFiction(livros) {
+    return  livros.filter((livro) => {
+       if (livro.genre === 'Ficção Científica' || livro.genre === 'Fantasia'){
+           return livro;
+       }
+    })
+  }
+  console.log(fantasyOrScienceFiction(books));
+  assert.deepStrictEqual(fantasyOrScienceFiction(books), expectedResult);
