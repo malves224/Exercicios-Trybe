e4de068a828e79f683227a046da1a1049c19250d
Matheus Alves <malves224@gmail.com>
2021-07-30 18:18:47 -0300

    soluçao exercicio 1 bloco 8.3

--- exercises/bloco_8/exercicio2.js
+++ exercises/bloco_8/exercicio2.js
@@ -62,4 +62,22 @@ const books = [
   },
 ];
 
-// Adicione o código do exercício aqui:
+const assert = require('assert');
+
+const expectedResult = [
+  'As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin',
+  'O Senhor dos Anéis - Fantasia - J. R. R. Tolkien',
+  'Fundação - Ficção Científica - Isaac Asimov',
+  'Duna - Ficção Científica - Frank Herbert',
+  'A Coisa - Terror - Stephen King',
+  'O Chamado de Cthulhu - Terror - H. P. Lovecraft',
+];
+
+function formatedBookNames(livros) {
+  let arrayOutput = livros.map((livro => {
+    return `${livro.name} - ${livro.genre} - ${livro.author.name}`
+  }))
+  return arrayOutput
+}
+
+assert.deepStrictEqual(formatedBookNames(books), expectedResult);
