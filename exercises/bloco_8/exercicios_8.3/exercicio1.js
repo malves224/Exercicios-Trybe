97182783e1f6cca53c1ade22d06c5416418b53a7
Matheus Alves <malves224@gmail.com>
2021-07-30 16:10:41 -0300

    refatorando o codigo usando .map

--- exercises/bloco_8/exercicios_8.3/exercicio1.js
+++ exercises/bloco_8/exercicios_8.3/exercicio1.js
@@ -1,12 +1,12 @@
 const products = ['Arroz', 'Feijão', 'Alface', 'Tomate'];
 const prices = [2.99, 3.99, 1.5, 2];
 
-const juntarArrays = products.map((value, index) => {
-    let objetoComPrice = {};
-    let arrayJunto = [];
-    objetoComPrice[value] = prices[index];
-    arrayJunto = objetoComPrice;
-    return arrayJunto;
-})
+const juntarArrays = (listaProdutos, precosProdutos) =>
+    listaProdutos.map((value, index) =>
+        ({
+            [value]: precosProdutos[index]
+        })
+    )
 
-console.log(juntarArrays.length);
+
+console.log(juntarArrays(products, prices));
