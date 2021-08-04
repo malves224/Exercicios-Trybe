e10bc9bba46f16570da61cfe004b24e0b8ea9138
Matheus Alves <malves224@gmail.com>
2021-08-04 13:33:14 -0300

    adiciona funçao que soma numeros pares de um array usando apenas o reduce

--- exercises/bloco_8/exercicios_reduce/exercicios_basicos.js
+++ exercises/bloco_8/exercicios_reduce/exercicios_basicos.js
@@ -1,6 +1,6 @@
 const numbers = [32, 15, 3, 2, -5, 56, 10];
 
-const sumNumbers = numbers.reduce((result, number) => result - number); // O parâmetro `result` é o acumulador. Ele recebe, do `reduce`, o retorno da função a cada iteração.
+//const sumNumbers = numbers.reduce((result, number) => result - number); // O parâmetro `result` é o acumulador. Ele recebe, do `reduce`, o retorno da função a cada iteração.
 //console.log(sumNumbers); // 113
 
 
@@ -11,7 +11,7 @@ const getBigger = (bigger, number) => ((bigger > number) ? bigger : number);
 const bigger = numbers2.reduce(getBigger, 0);
 // console.log(bigger); // 101
 
-const numbers3 = [18, 19, 23, 53, 4, 5, 76, 23, 54];
+const numbers3 = [2, 19, 23, 53, 2, 5, 2, 23, 2];
 
 const getEven = (number) => number % 2 === 0
 
@@ -19,4 +19,7 @@ const sumPares = (acc, number) => acc + number;
 
 const sumNumbersPares = (numbers) =>  numbers.filter((getEven)).reduce(sumPares);
 
-console.log(sumNumbersPares(numbers3));
+//console.log(sumNumbersPares(numbers3)); // 152
+
+const sumNumbers = (array) => array.reduce((acc, number) => ((number % 2 === 0 ? acc + number : acc)),0);
+//console.log(sumNumbers(numbers3)) // 8
