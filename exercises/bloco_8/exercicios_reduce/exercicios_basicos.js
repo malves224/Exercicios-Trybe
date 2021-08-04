8df4a049169098fd382b5229034ca6029d106174
Matheus Alves <malves224@gmail.com>
2021-08-04 13:07:18 -0300

    adiciona funçao getBigger que e uma callback para funçao .reduce que returna o maior numero do array que o reduce esta percorrendo

--- exercises/bloco_8/exercicios_reduce/exercicios_basicos.js
+++ exercises/bloco_8/exercicios_reduce/exercicios_basicos.js
@@ -1,6 +1,12 @@
 const numbers = [32, 15, 3, 2, -5, 56, 10];
 
-const sumNumbers = numbers.reduce((result, number) => result + number); // O parâmetro `result` é o acumulador. Ele recebe, do `reduce`, o retorno da função a cada iteração.
-console.log(sumNumbers); // 113
+const sumNumbers = numbers.reduce((result, number) => result - number); // O parâmetro `result` é o acumulador. Ele recebe, do `reduce`, o retorno da função a cada iteração.
+//console.log(sumNumbers); // 113
 
-// Ou seja:
+
+const numbers2 = [101, 85, -30, 3, 99];
+
+const getBigger = (bigger, number) => ((bigger > number) ? bigger : number);
+
+const bigger = numbers2.reduce(getBigger, 0);
+console.log(bigger); // 101
