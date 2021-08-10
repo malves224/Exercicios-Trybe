6cdaf8e18123dbc876c405d729e24d4e7a1ad064
Matheus Alves <malves224@gmail.com>
2021-08-10 18:33:09 -0300

    adiciona tratamento de erro

--- exercises/bloco_9/js_assincrono_callbacks/exerciciosBasicos.js
+++ exercises/bloco_9/js_assincrono_callbacks/exerciciosBasicos.js
@@ -10,23 +10,32 @@ pushNumber(numbers, 3);
 
 //setTimeout(() => assert.deepStrictEqual(numbers, [2, 3, 1]), 3000);
 
-const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
-const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+const countryName = ({ name }) => console.log(`Returned country is ${name}`);
+const countryCurrency = ({ name, currency }) => console.log(`${name}'s currency is the ${currency}`);
 
 const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMilliseconds);
 
+const printErrorMessage = (error) => console.log(`Error getting country: ${error}`);
 
-const getUser = (callback) => {
-    setTimeout(() => {
-      const user = {
-        firstName: "Ivan",
-        lastName: "Ivanovich",
-        nationality: "Russian",
+const getCountry = (onSuccess) => {
+  setTimeout(() => {
+    const didOperationSucceed = Math.random() >= 0.5;
+    if(didOperationSucceed) {
+      const country = {
+        name: "Brazil",
+        hdi: 0.759,
+        currency: "Real",
       };
-      console.log(callback(user));
-    }, delay());
-  };
+      onSuccess(country);
+    }
+    else {
+        printErrorMessage('erro ao encontrar o Pais')
+    }
+  }, delay());
+};
 
+// Deve imprimir "Returned country is Brazil" no sucesso, ou "Error getting country: Country could not be found" em falha
+getCountry(countryName, printErrorMessage);
 
-getUser(userFullName); // deve imprimir "Hello! My name is Ivan Ivanovich" depois de um certo tempo
-getUser(userNationality); // deve imprimir "Ivan is Russian" depois de um certo tempo
+// Deve imprimir "Brazil's currency is the Real" no sucesso, ou "Error getting country: Country could not be found" em falha
+//getCountry(countryCurrency, printErrorMessage);
