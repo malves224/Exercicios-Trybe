c05c68d0e8b134db146aca0d75e33352c0470137
Matheus Alves <malves224@gmail.com>
2021-08-10 16:05:11 -0300

    exercicio de callback

--- exercises/bloco_9/js_assincrono_callbacks/exerciciosBasicos.js
+++ exercises/bloco_9/js_assincrono_callbacks/exerciciosBasicos.js
@@ -4,8 +4,24 @@ const pushNumber = (list, number) => list.push(number);
 
 let numbers = [];
 
-setTimeout(() => pushNumber(numbers, 1), 3000);
+//setTimeout(() => pushNumber(numbers, 1), 3000);
 pushNumber(numbers, 2);
 pushNumber(numbers, 3);
 
-setTimeout(() => assert.deepStrictEqual(numbers, [2, 3, 1]), 3000);
+//setTimeout(() => assert.deepStrictEqual(numbers, [2, 3, 1]), 3000);
+
+const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+
+const getUser = (callback) => {
+  const userToReturn = {
+    firstName: "Ivan",
+    lastName: "Ivanovich",
+    nationality: "Russian",
+  };
+  return callback(userToReturn);
+};
+
+
+assert.strictEqual(getUser(userFullName), "Hello! My name is Ivan Ivanovich"); // complete a chamada da função de getUser
+assert.strictEqual(getUser(userNationality), "Ivan is Russian"); // complete a chamada da função de getUser
