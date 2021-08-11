6899cdb910582ab27fb3c416d378197a7d70e513
Matheus Alves <malves224@gmail.com>
2021-08-11 18:05:10 -0300

    adiciona exercicio 3

--- exercises/bloco_9/js_assincrono_callbacks/exercicio3.js
+++ exercises/bloco_9/js_assincrono_callbacks/exercicio3.js
@@ -5,15 +5,19 @@ const getMarsTemperature = () => {
   return Math.floor(Math.random() * maxTemperature);
 };
 
+const toFahrenheit = (degreeCelsius) => (degreeCelsius * 9/5) + 32;
+
+const temperatureInFahrenheit = (temperature) => console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
+
+const greet = (temperature) => console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
+
 // crie a função sendMarsTemperature abaixo
 
-const sendMarsTemperature = (mensagen) => { 
-	setTimeout(()=> {
-		console.log(`Mars temperatu is: ${mensagen()} degree Celsius`);
-	}, messageDelay());
+const sendMarsTemperature = (callback) => { 
+	callback
 }
 
 sendMarsTemperature(getMarsTemperature); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
 
-console.log('eu cheguei primeiro eheheheheheh');
-console.log('e eu em segundo uhuu')
+sendMarsTemperature(temperatureInFahrenheit); // imprime "It is currently 47ºF at Mars", por exemplo
+sendMarsTemperature(greet); // imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo
