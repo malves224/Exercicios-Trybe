91f8e3645dee527ffe4775905999492f729a2cfd
Matheus Alves <malves224@gmail.com>
2021-08-19 16:09:17 -0300

    exercicio 2 de testes unitarios so que com jest

--- exercises/bloco_10/exercicios10_1/exercicio_1a_5_unit.test.js
+++ exercises/bloco_10/exercicios10_1/exercicio_1a_5_unit.test.js
@@ -15,3 +15,34 @@ describe('1º sum que soma os 2 parametros, apenas se os parametros forem number
     expect(() => { sum(1,'1')}).toThrow();
   });
 });
+
+function myRemove(arr, item) {
+  let newArr = [];
+  for (let index = 0; index < arr.length; index += 1) {
+    if (item !== arr[index]) {
+      newArr.push(arr[index]);
+    }
+  }
+  return newArr;
+}
+
+describe('2º função myRemove(arr, item) recebe um array e retorna um novo array sem o elemento item', () => { 
+  const arrayOld = [1, 2, 3, 4];
+  test('Verifica se retorna um novo array sem o elemento item', () => { 
+    expect(myRemove(arrayOld, 3)).toEqual([1, 2, 4]);
+
+  });
+
+  test('Verifica se a função nao retorna o array novo sem remover o item', () => { 
+    expect(myRemove(arrayOld, 3)).not.toEqual([1, 2, 3, 4]);
+  });
+
+  test('verifica se o array passado como parametro nao sofreu alterações', () => { 
+    myRemove(arrayOld, 3);
+    expect(arrayOld).toEqual([1, 2, 3, 4]);
+  });
+  
+  test('verifica se a chamada myRemove([1, 2, 3, 4], 5) retorno o array esperado', () => {
+    expect(myRemove(arrayOld, 5)).toEqual([1, 2, 3, 4]);
+  })
+});
