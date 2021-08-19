85fd76ff37b4c67bc0c9599f4c4e4738ff4a6daf
Matheus Alves <malves224@gmail.com>
2021-08-19 16:31:20 -0300

    cria teste para verificar se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 3) retorna o array esperado

--- exercises/bloco_10/exercicios10_1/exercicio_1a_5_unit.test.js
+++ exercises/bloco_10/exercicios10_1/exercicio_1a_5_unit.test.js
@@ -44,5 +44,24 @@ describe('2º função myRemove(arr, item) recebe um array e retorna um novo arr
   
   test('verifica se a chamada myRemove([1, 2, 3, 4], 5) retorno o array esperado', () => {
     expect(myRemove(arrayOld, 5)).toEqual([1, 2, 3, 4]);
+  });
+});
+
+function myRemoveWithoutCopy(arr, item) {
+  for (let index = 0, len = arr.length; index < len; index += 1) {
+    if (arr[index] === item) {
+      arr.splice(index, 1);
+      index -= 1;
+      len -= 1;
+    }
+  }
+
+  return arr;
+}
+
+describe('3º A função myRemoveWithoutCopy recebe um array arr e retorna o próprio array sem o elemento item caso ele exista no array', () => { 
+  let numbersList = [1, 2, 3, 4];
+  test('verifica se a ao chamar a função com o primeiro parametro um array, e o seguindo item para remover, retorna o mesmo array sem o item', () => {
+    expect(myRemoveWithoutCopy(numbersList, 3)).toEqual([1, 2, 4]);
   })
 });
