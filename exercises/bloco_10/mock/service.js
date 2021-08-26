34b225e75117b954bb0877af98dcfa1a961cf592
Matheus Alves <malves224@gmail.com>
2021-08-26 20:36:53 -0300

    adiciona exercicio do jest.fn()

--- exercises/bloco_10/mock/service.js
+++ exercises/bloco_10/mock/service.js
@@ -1,7 +1,7 @@
 function randomRgbColor() {
-    const r = Math.floor(Math.random() * 256);
-    const g = Math.floor(Math.random() * 256);
-    const b = Math.floor(Math.random() * 256);
+    const r = Math.floor(Math.random() * 2516);
+    const g = Math.floor(Math.random() * 2516);
+    const b = Math.floor(Math.random() * 2516);
     return `rgb(${r}, ${g}, ${b})`;
   }
   module.exports = { randomRgbColor };
--- exercises/bloco_10/mock/service.test.js
+++ exercises/bloco_10/mock/service.test.js
@@ -1,7 +1,21 @@
 const service = require('./service');
 
 test("#randomRgbColor", () => {
-  // testando se a função foi chamada
-  service.randomRgbColor();
-  expect(service.randomRgbColor).toHaveBeenCalled();
+ // testando quantas vezes a função foi chamada e qual seu retorno
+ service.randomRgbColor = jest
+ .fn()
+ .mockReturnValue('default value')
+ .mockReturnValueOnce('first call')
+ .mockReturnValueOnce('second call');
+
+expect(service.randomRgbColor).toHaveBeenCalledTimes(0);
+
+expect(service.randomRgbColor()).toBe("first call");
+expect(service.randomRgbColor).toHaveBeenCalledTimes(1);
+
+expect(service.randomRgbColor()).toBe("second call");
+expect(service.randomRgbColor).toHaveBeenCalledTimes(2);
+
+expect(service.randomRgbColor()).toBe("default value");
+expect(service.randomRgbColor).toHaveBeenCalledTimes(3);
 });
