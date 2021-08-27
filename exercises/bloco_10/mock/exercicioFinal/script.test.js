38408e2fd292faf9e39f691f86c1ca0d0e32fcfc
Matheus Alves <malves224@gmail.com>
2021-08-27 19:03:55 -0300

    adiciona teste e simula comportamente diferenciado uma unica vez para funçao randownNUmber

--- exercises/bloco_10/mock/exercicioFinal/script.test.js
+++ exercises/bloco_10/mock/exercicioFinal/script.test.js
@@ -1,10 +1,23 @@
 const script = require('./script');
 
-script.randownNumber = jest.fn().mockReturnValue(15);
+
+
 
 describe('testa comportamento da função randowNumber', () => {
+    script.randownNumber = jest.fn().mockReturnValue(15)
+
     it('verfica se a função foi chamada ', () => {
-        //script.randownNumber();
+        script.randownNumber();
         expect(script.randownNumber).toHaveBeenCalled();
+        expect(script.randownNumber).toHaveBeenCalledTimes(1);
+    });
+    it('verifica se a função retorna o valor esperado', () => {
+        expect(script.randownNumber()).toEqual(15);
+    });
+    it('verifica se a função randownNumber esta dividindo o parametro pelo outro', () => { 
+        script.randownNumber = jest.fn().mockImplementationOnce((a, b) => a / b);
+        expect(script.randownNumber(2, 2)).toBe(1);
     })
-})
+
+});
+
