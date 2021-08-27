1c1479bff880c11ba10ca8459bf20904a9392a0d
Matheus Alves <malves224@gmail.com>
2021-08-27 20:49:10 -0300

    adiciona mockimplementatio para funçao concString

--- exercises/bloco_10/mock/exercicioFinal/script.js
+++ exercises/bloco_10/mock/exercicioFinal/script.js
@@ -1,7 +1,14 @@
 const randownNumber = () => {
     return Math.trunc(Math.random() * 100)
-}
+};
+
+const upCaseString = (string) => string.toUpperCase();
+const primeiraLetra = (string) => string[0];
+const concString = (string1, string2) => string1 + string2;
 
 module.exports = {
-    randownNumber
+    randownNumber,
+    primeiraLetra,
+    concString,
+    upCaseString,
 };
