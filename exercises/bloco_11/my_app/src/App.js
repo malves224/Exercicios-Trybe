81859d31a93c22ca8e80e00b8044296995297753
Matheus Alves <malves224@gmail.com>
2021-08-31 15:39:15 -0300

    cria pasta para exercicios do bloco 11 react

---
+++ exercises/bloco_11/my_app/src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import HelloWorld from './HelloWorld';
+
+function App() { 
+}
+
+export default App;
---
+++ exercises/bloco_11/my_app/src/Component.js
@@ -0,0 +1,14 @@
+import React, {
+	Component
+} from 'react'
+
+class HelloWorld extends Component {
+	render() {
+		return
+			<div >
+				<h1><p> Matheus </p></h1>
+			</div>	
+	}
+}
+
+export default HelloWorld;
