f8b7aca07f5aa35dbb0810caba466513eef2a336
Matheus Alves <malves224@gmail.com>
2021-09-02 16:29:31 -0300

    cria button com onClick

--- exercises/bloco_12/exercicios-bloco12/src/App.js
+++ exercises/bloco_12/exercicios-bloco12/src/App.js
@@ -1,9 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {
+  Component
+} from 'react';
 
-function App() {
-  return (
-  );
+function imprimeText (message) { 
+  console.log(message);
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <button onClick={ imprimeText }> Butaum </button>
+        <button onClick={ imprimeText }> butaumzin </button>
+        <button onClick={ imprimeText }> Butaumzão </button>
+      </div>
+    );
+  }
 }
 
 export default App;
