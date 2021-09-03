38110843c6e211eb249d29d449f967654bf08ec4
Matheus Alves <malves224@gmail.com>
2021-09-03 16:05:25 -0300

    adiciona formulario e torna o componente select controlado

--- exercises/bloco_12/exercicios-bloco12/src/App.js
+++ exercises/bloco_12/exercicios-bloco12/src/App.js
@@ -2,19 +2,47 @@ import './App.css';
 import React, {
   Component
 } from 'react';
-
-function imprimeText (message) { 
-  console.log(message);
-}
+import './style.css';
 
 class App extends Component {
+  constructor() { 
+    super();
+    this.handleChage = this.handleChage.bind(this);
+
+    this.state = { 
+      saudacao: '',
+    }
+  } 
+
+  handleChage(event) { 
+    this.setState({ 
+      saudacao: event.target.value,
+    })
+  }
   render() {
     return (
-      <div>
-        <button onClick={ imprimeText }> Butaum </button>
-        <button onClick={ imprimeText }> butaumzin </button>
-        <button onClick={ imprimeText }> Butaumzão </button>
-      </div>
+      <form className="form"> 
+        <label> 
+        escolha uma saudação:
+
+            <select 
+            onChange={this.handleChage}
+            value={ this.state.saudacao }
+            >  
+              <option> OLá</option>
+              <option> oi </option>
+              <option> érr </option>
+            </select>
+        </label>
+        <label> 
+          marque essa opção
+          <input type="checkbox" />
+        </label>
+        <label> 
+          escreva algo
+          <input type="textarea" /> 
+        </label>
+      </form>
     );
   }
 }
