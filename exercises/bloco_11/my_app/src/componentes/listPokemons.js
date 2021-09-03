9a5f6752117bf3ba58281deac136b89bc4d10b79
Matheus Alves <malves224@gmail.com>
2021-09-03 14:01:15 -0300

    adiciona chave no state para armazenar o valor digitado no input

--- exercises/bloco_11/my_app/src/componentes/listPokemons.js
+++ exercises/bloco_11/my_app/src/componentes/listPokemons.js
@@ -4,24 +4,33 @@ import '../style.css';
 
 class PokemonsList extends React.Component {
   constructor() { 
-    super();
-    this.teste = this.teste.bind(this) // isso é para que a função teste começe a acessar o this
-    console.log('oiii')
+    super(); // A função `super()` é chamada para garantir que a lógica interna do React rode **antes** da sua. Se não for assim, o código não funcionará
+    this.handleChange = this.handleChange.bind(this) // isso é para que a função teste começe a acessar o this
+    this.state = { 
+      valueSearch: '',
+    }
+    console.log(this);
   }
 
-  teste() { 
-    console.log(this);
-    console.log('tec')
+   handleChange(event) { 
+    const search = document.querySelector('input').value; 
+     this.setState({ 
+      valueSearch: search ,
+    });
   }
 	render() {
 		const { pokemonsData } = this.props;
+    const { valueSearch } = this.state;
+    const valueSearchUperCase = valueSearch.toUpperCase();
 		return( 
       <main className="main">
         <h1 className="title"> Pokemons </h1>
-        <input onChange={ this.teste } type="text" />
+        <input onChange={ this.handleChange } type="text" />
         <div className="listPokemons" >
-			  	{pokemonsData.map((pokemon) => <PokemonComponent className="cardsPokemons" 
-           key={pokemon.id} pokemonsInfo={pokemon} />)}
+			  	{ pokemonsData.filter((pokemon) => pokemon.name.toUpperCase()
+          .includes(valueSearchUperCase))
+          .map((pokemon) => <PokemonComponent className="cardsPokemons" 
+           key={pokemon.id} pokemonsInfo={pokemon} />) }
 		  	</div>
       </main>  
 			);
