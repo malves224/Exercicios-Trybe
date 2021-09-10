269f34803fa26c37927deb64af6de880e40e6f87
Matheus Alves <malves224@gmail.com>
2021-09-10 15:58:17 -0300

    remove elementos padrao do App.js

---
+++ exercises/bloco_13/my-interdimensional-app/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+class App extends Component {
+  constructor(props) { 
+    super(props);
+
+  }
+  render() {
+    return (
+
+    );
+  }
+}
+
+export default App;
