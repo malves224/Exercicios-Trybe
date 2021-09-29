27d01f1ed94803ee371d5fe7d34bfbc45f563308
Matheus Alves <malves224@gmail.com>
2021-09-29 13:18:26 -0300

    adiciona teste que verifica ao clicar em link se e redirecionado para rota correta

--- exercises/bloco_14/RTL-first-step/exercicios14.3/testrouter/src/App.test.js
+++ exercises/bloco_14/RTL-first-step/exercicios14.3/testrouter/src/App.test.js
@@ -1,8 +1,22 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import renderWithRouter from './renderWithRouter';
+import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('deve renderizar o componenete App', () => {
+  renderWithRouter(<App />);
+  const home = screen.getByText(/você está na página início/i);
+  expect(home).toBeInTheDocument();
 });
+
+test('deve renderizar o componenete sobre ', () => { 
+  const { history } = renderWithRouter(<App />);
+  const linkSobre = screen.getByText(/Sobre/i);
+  userEvent.click(linkSobre);
+  const pathname = history.location.pathname;
+  expect(pathname).toBe('/about');
+
+  const aboutAllELement = screen.getByText(/Você está na página Sobre/);
+  expect(aboutAllELement).toBeInTheDocument();
+})
