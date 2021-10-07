68c984e7cf35e16133d819208c151060507186b3
Matheus Alves <malves224@gmail.com>
2021-10-07 12:05:17 -0300

    cria estrutura do redux

---
+++ exercises/bloco_15/estados_em_redux_15.1/exercicioBasico1.js
@@ -0,0 +1,30 @@
+import Redux from "redux";
+
+const fazerLogin = (email) => ({
+  type: "LOGIN",
+  email
+});
+
+const ESTADO_INICIAL = {
+  login: false,
+  email: '',
+}
+
+const reducer = (state = ESTADO_INICIAL, action) => {
+  switch (action.type) {
+    case "LOGIN":
+      return {
+        ...state,
+        login: !state.login,
+        email: action.email,
+      };
+      default: // no switch, sempre precisamos ter um caso default!
+      return state;
+  }
+}
+
+const store = Redux.createStore(reducer);
+
+store.dispatch(fazerLogin("alguem@email.com"));
+
+console.log(store.getState());
