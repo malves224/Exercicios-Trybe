255c562322bdf89dba27f8e632219b724e8b75fd
Matheus Alves <malves224@gmail.com>
2021-10-28 13:59:14 -0300

    exercise on complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Cars from './Cars';
+import MyContext from './context';
 
-function App() {
-  return (
-    <Cars />
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      cars: {
+        red: false,
+        blue: false,
+        yellow: false,
+      },
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(car) {
+    console.log(car); 
+    this.setState((previousCar) => ({
+      cars: {
+        ...this.state.cars,
+        [car]: !previousCar.cars[car],
+      },
+    }));
+  }
+
+  render() {
+    return (
+      <MyContext.Provider value={{...this.state, handleChange: this.handleChange}}>
+        <Cars />
+      </MyContext.Provider>
+    );
+    }
 }
 
 export default App;
