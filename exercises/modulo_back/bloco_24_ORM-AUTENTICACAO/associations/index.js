2b725b6df5765046413f684782cb7d47ee923c00
Matheus Alves <malves224@gmail.com>
2022-02-03 12:19:13 -0300

    adiciona post, utilizando transaction unmanaged

--- exercises/modulo_back/bloco_24_ORM-AUTENTICACAO/associations/index.js
+++ exercises/modulo_back/bloco_24_ORM-AUTENTICACAO/associations/index.js
@@ -1,8 +1,14 @@
 // index.js
 const express = require('express');
-const { Address, Employee } = require('./models');
+const { Address, Employee, Book, User } = require('./models');
+const bodyParser = require('body-parser');
+const Sequelize = require('sequelize');
+const config = require('./config/config');
+
 
 const app = express();
+app.use(bodyParser.json());
+const sequelize = new Sequelize(config.development);
 
 app.get('/employees', async (_req, res) => {
   try {
@@ -15,6 +21,35 @@ app.get('/employees', async (_req, res) => {
     console.log(e.message);
     res.status(500).json({ message: 'Ocorreu um erro' });
   };
+});
+
+app.post('/employees', async (req, res) => { // caso do uso, utilizando transaction
+  const transaction = await sequelize.transaction();
+  try {
+    const { firstName, lastName, age, city, street, number } = req.body;
+
+    const employee = await Employee.create(
+      { firstName, lastName, age },
+      { transaction }
+      );
+
+    await Address.create(
+      { city, street, number, employeeId: employee.id },
+      { transaction }
+      );
+
+    // Se chegou até essa linha, quer dizer que nenhum erro ocorreu.
+    // Com isso, podemos finalizar a transação usando a função `commit`.
+    await transaction.commit();
+
+    return res.status(201).json({ message: 'Cadastrado com sucesso' },);
+  } catch (e) {
+    await transaction.rollback();
+    // Se entrou nesse bloco é porque alguma operação falhou.
+    // Nesse caso, o sequelize irá reverter as operações anteriores com a função rollback, não sendo necessário fazer manualmente
+    console.log(e.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  }
 });
 
 app.get('/employeesAndAddresses/:id', async (req, res) => { // eager loading, retornando employees com seus endereços.
@@ -51,6 +86,24 @@ app.get('/employees/:id', async (req, res) => { // Lazzi loading, retornando tod
     }
 
     return res.status(200).json(employee);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  };
+});
+
+app.get('/usersbooks/:id', async (req, res) => {// exemplo de utilização muitos para muitos, olhe os models para maior clareza.
+  try {
+    const { id } = req.params;
+    const user = await User.findOne({
+      where: { userId: id },
+      include: [{ model: Book, as: 'books', through: { attributes: [] } }],
+    });
+
+    if (!user)
+      return res.status(404).json({ message: 'Usuário não encontrado' });
+
+    return res.status(200).json(user);
   } catch (e) {
     console.log(e.message);
     res.status(500).json({ message: 'Algo deu errado' });
