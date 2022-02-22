a29ac09ca7c2c39866c5b1120959948a64c8bfb2
Matheus Alves <malves224@gmail.com>
2022-02-22 11:40:34 -0300

    exemplo de aplicaçao node express com typescript

---
+++ exercises/modulo_back/bloco_26_typescript/node_com_typescript_3/dist/index.js
@@ -0,0 +1,18 @@
+"use strict";
+// ./index.ts
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const books_1 = __importDefault(require("./routes/books"));
+const app = express_1.default.application = (0, express_1.default)();
+const PORT = 8000;
+app.use(express_1.default.json());
+app.get('/', (req, res) => {
+    res.send('Express + TypeScript');
+});
+app.use(books_1.default);
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
