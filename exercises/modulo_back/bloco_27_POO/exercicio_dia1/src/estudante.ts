55db7c3d2658d3eedd566df60029301e3ae6ad95
Matheus Alves <malves224@gmail.com>
2022-03-08 13:02:44 -0300

    classe que tras relatorio dos pedidos

--- exercises/modulo_back/bloco_27_POO/exercicio_dia1/src/estudante.ts
+++ exercises/modulo_back/bloco_27_POO/exercicio_dia1/src/estudante.ts
@@ -1,12 +1,58 @@
-class Estudante {
-  private _nome: string;
+abstract class People {
+   private _name: string = String()
+   private _birthDate: Date = new Date();
+
+  constructor(name: string, birthDate: Date,) {
+    this.name = name;
+    this.birthDate = birthDate;
+  }
+
+  get name() {
+    return this._name;
+  }
+  set name(data: string) {
+    if (data.length < 2){
+      console.log('o nome deve ter no minimo 3 caracteres');
+    }
+    this._name = data;
+  }
+
+  get birthDate() {
+    return this._birthDate;
+  }
+
+  getAge(): number {
+    const date = new Date();
+    const age = date.getFullYear() - this._birthDate.getFullYear();
+    return age;
+  }
+
+  set birthDate(data: Date) {
+    const date = new Date();
+    const dateToday = `${date.getDate()}${date.getMonth() +1}${date.getFullYear()}`;
+    const dateBirthDate = `${data.getDate()}${data.getMonth() +1}${data.getFullYear()}`;
+    const age = date.getFullYear() - data.getFullYear();
+    if (age < 0) {
+       console.log('data nao pode ser no futuro');
+    } else if (age >= 120) {
+      console.log('idade nao pode ser superior a 120');
+    } else {
+      this._birthDate = data;
+    }
+  }
+}
+
+class Estudante extends People {
   private _matricula: string;
   private _notasDeProva: [number, number, number, number];
   private _notasDeTrabalho: [number, number];
 
-  constructor(nome: string, matricula: string, 
-    notasDeProva: [number, number, number, number], notasDeTrabalho: [number, number]) {
-    this._nome = nome;
+  constructor(matricula: string, 
+    notasDeProva: [number, number, number, number], 
+    notasDeTrabalho: [number, number],
+    name: string, birthDate: Date
+    ) {
+    super(name, birthDate);
     this._matricula = matricula;
     this._notasDeProva = notasDeProva;
     this._notasDeTrabalho = notasDeTrabalho;
@@ -26,7 +72,9 @@ class Estudante {
   }
 }
 
-const joao = new Estudante('joão', '125', [3, 5, 8, 4], [6, 8]);
+
+
+// const joao = new Estudante('joão', '125', [3, 5, 8, 4], [6, 8]);
 // console.log(joao);
-console.log(joao.avgNotas);
+// console.log(joao.avgNotas);
 
--- exercises/modulo_back/bloco_27_POO/exercicio_dia1/src/pedidoLanchonete.ts
+++ exercises/modulo_back/bloco_27_POO/exercicio_dia1/src/pedidoLanchonete.ts
@@ -1,21 +1,20 @@
-
 class Client {
   private _name: string = String();
 
   constructor(name: string) {
-      this.name = name;
+    this.name = name;
   }
 
   get name(): string {
-      return this._name;
+    return this._name;
   }
 
   set name(value: string) {
-      if (value.length < 3) {
-          throw new Error('O nome deve conter no mínimo 3 caracteres.');
-      }
+    if (value.length < 3) {
+      throw new Error('O nome deve conter no mínimo 3 caracteres.');
+    }
 
-      this._name = value;
+    this._name = value;
   }
 }
 
@@ -25,100 +24,122 @@ class OrderItem {
   private _price: number = Number();
 
   constructor(name: string, price: number) {
-      this.name = name;
-      this.price = price;
+    this.name = name;
+    this.price = price;
   }
 
   get name(): string {
-      return this._name;
+    return this._name;
   }
 
   set name(value: string) {
-      if (value.length < 3) {
-          throw new Error('O nome deve conter no mínimo 3 caracteres.');
-      }
-
-      this._name = value;
+    if (value.length < 3) {
+      throw new Error('O nome deve conter no mínimo 3 caracteres.');
+    }
+    this._name = value;
   }
 
   get price(): number {
-      return this._price;
+    return this._price;
   }
 
   set price(value: number) {
-      if (value < 0) throw new Error('O preço deve ser positivo.');
+    if (value < 0) throw new Error('O preço deve ser positivo.');
 
-      this._price = value;
+    this._price = value;
   }
+}
+
+enum PaymentMethod {
+  DINHEIRO = 'dinheiro',
+  CARTAO = 'cartão',
+  VALE = 'vale'
 }
 
 class Order {
   private _client: Client;
   private _items: OrderItem[] = [];
-  private _paymentMethod: string;
+  private _paymentMethod: PaymentMethod;
   private _discount: number = 0;
 
-  constructor(client: Client, items: OrderItem[], paymentMethod: string, discount: number) {
-      this._client = client;
-      this.items = items;
-      this._paymentMethod = paymentMethod;
-      this.discount = discount;
+  constructor(client: Client, items: OrderItem[], paymentMethod: PaymentMethod, discount: number) {
+    this._client = client;
+    this.items = items;
+    this._paymentMethod = paymentMethod;
+    this.discount = discount;
   }
 
   get client(): Client {
-      return this._client;
+    return this._client;
   }
 
   set client(value: Client) {
-      this._client = value;
+    this._client = value;
   }
 
   get items(): OrderItem[] {
-      return this._items;
+    return this._items;
   }
 
   set items(value: OrderItem[]) {
-      if (value.length === 0) {
-          throw new Error('O pedido deve ter pelo meno um item.');
-      }
+    if (value.length === 0) {
+      throw new Error('O pedido deve ter pelo menos um item.');
+    }
 
-      this._items = value;
+    this._items = value;
   }
 
-  get paymentMethod(): string {
-      return this._paymentMethod;
+  get paymentMethod(): PaymentMethod {
+    return this._paymentMethod;
   }
 
-  set paymentMethod(value: string) {
-      this._paymentMethod = value;
+  set paymentMethod(value: PaymentMethod) {
+    this._paymentMethod = value;
   }
 
   get discount(): number {
-      return this._discount;
+    return this._discount;
   }
 
   set discount(value: number) {
-      if (value < 0) {
-          throw new Error('O disconto não pode ser um valor negatívo.');
-      }
+    if (value < 0) {
+      throw new Error('O disconto não pode ser um valor negatívo.');
+    }
 
-      this._discount = value;
+    this._discount = value;
   }
 
   calculateTotal(): number {
-      return this.items.
-          reduce((previousValue, item) => {
-              const total = previousValue += item.price;
-              
-              return total;
-          }, 0)
+    return this.items.
+      reduce((previousValue, item) => {
+        const total = previousValue += item.price;
+
+        return total;
+      }, 0)
   }
 
   calculateTotalWithDiscount(): number {
-      return this.calculateTotal() * (1 - this.discount);
+    return this.calculateTotal() * (1 - this.discount);
   }
 
 }
+
+class OrderRepository {
+  private _orders: Order[] = [];
+  constructor(orders: Order[]) {
+    this._orders = orders;
+  }
+
+  get orders(): Order[] {
+    return this._orders;
+  }
+
+  addOrder(order: Order) {
+    this._orders.push(order);
+  }
+
+}
+
 
 // Para testar!
 
@@ -128,7 +149,7 @@ const sandwiche = new OrderItem('Sandwiche Natural', 5.00);
 const juice = new OrderItem('Suco de Abacaxí', 5.00);
 const dessert = new OrderItem('Gelatina de Uva', 2.50);
 
-const order = new Order(client, [sandwiche, juice, dessert], 'dinheiro', 0.10);
+const order = new Order(client, [sandwiche, juice, dessert], PaymentMethod.DINHEIRO, 0.10);
 
 console.log(order);
 console.log('Valor normal: ', order.calculateTotal());
